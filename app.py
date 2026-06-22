ee81b689ac6985bd1e2ba172b5352deb4f67ece2
fengshangjiong <16979662@qq.com>
2026-06-22 16:31:20 +0800

    feat(home):新增小记首页备案号

--- app.py
+++ app.py
@@ -4315,7 +4315,7 @@ INDEX_HTML = r'''<!doctype html>
         <div class="brand">
           <img class="brand-avatar" src="/res/meimei-avatar.png" alt="槑槑头像">
           <div class="brand-copy">
-            <h1>AI槑槑 <span class="app-version">v2.2.17</span></h1>
+            <h1>AI槑槑 <span class="app-version">v2.2.18</span></h1>
             <span id="health">连接中</span>
           </div>
         </div>
